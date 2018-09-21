3c6f88c7d7949027aa85d3f15de9e8565c81d5a8
Alex Kladkov <san4ezmoney@ya.ru>
2018-09-21 21:37:47 +0300

    Add files via upload

--- src/containers/SearchScreen.js
+++ src/containers/SearchScreen.js
@@ -14,7 +14,8 @@ class SearchScreen extends Component{
                 onSearchInput={(searchPhrase) => {
                     this.setSearchPhrase(searchPhrase);
                 }}
-                onSearchStart={() => {
+                onSearchStart={(e) => {
+                    e.preventDefault();
                     this.props.fetchMovie()
                 }}
                 ></SearchComponent>
